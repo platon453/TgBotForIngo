71921baa85cd68afc0c012f2256f26ab573ad6df
skittles <65069112+platon453@users.noreply.github.com>
2025-11-16 16:30:37 +0300

    нерабочий бот

---
+++ bot/states.py
@@ -0,0 +1,7 @@
+# User states storage
+user_states = {} # {user_id: state_name}
+
+# State constants
+STATE_AWAIT_MAIN_MENU = 'await_main_menu'
+STATE_CATEGORY_SELECTED = 'category_selected'
+STATE_QUESTION_SELECTED = 'question_selected'
