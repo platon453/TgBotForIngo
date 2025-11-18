01c6cf302dc22d60ad9175044236d85b8a41ae10
skittles <65069112+platon453@users.noreply.github.com>
2025-11-19 00:24:17 +0300

    Решил проблему с config.py

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
+
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+dotenv_path = os.path.join(BASE_DIR, '.env')
+load_dotenv(dotenv_path=dotenv_path)
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-LANDING_URL = os.getenv("LANDING_URL", "https://www.ingos.ru/ingostart/")
+LANDING_URL = os.getenv("LANDING_URL", "https://www.ingos.ru/internship/")
+
